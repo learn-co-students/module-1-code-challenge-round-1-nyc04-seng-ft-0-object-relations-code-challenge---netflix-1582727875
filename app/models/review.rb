823183c532ea1f35ed36f0369cea82ac24b9404e
Andy Cruz <Andycruz008@gmail.com>
2020-02-26 10:52:15 -0500

    hi

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,40 @@
 class Review
+attr_accessor :rating,:viewer,:movie
+@@all = []
+    def initialize (viewer,movie,rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def rating
+        @rating
+    end
+
+    def viewer
+        @viewer
+    end
+
+    def movie
+        @movie
+    end
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 end
