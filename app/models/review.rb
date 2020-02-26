0fa2f9f01cb3bbb0460c22e2ed10574bf17350ab
erocknine <echeungsk@gmail.com>
2020-02-26 10:49:12 -0500

    eric code challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,19 @@
-class Review
+require_relative 'movie.rb'
+require_relative 'viewer.rb'
 
+class Review
+    attr_accessor :viewer, :movie, :rating
+    @@all = []
+
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,3 +1,6 @@
+require_relative 'review.rb'
+require_relative 'movie.rb'
+
 class Viewer
   attr_accessor :username
 
@@ -6,6 +9,30 @@ class Viewer
   def initialize(username)
     @username = username
     self.class.all << self
+  end
+
+  def reviews
+    Review.all.select{|reviews|reviews.viewer == self} #== self
+  end
+
+  def reviewed_movies
+    reviewed = Review.all.select{|reviews|reviews.viewer == self}
+    reviewed.map{|reviews| "#{self.username} has reviewed #{reviews.movie.title}."}
+  end
+
+  def reviewed_movie?(movie_instance)
+    reviewed = Review.all.select{|reviews|reviews.viewer == self}
+    reviewed.find{|reviews|reviews.movie == movie_instance} ? true : false
+  end
+
+  def rate_movie(movie_instance, rating)
+    if reviewed_movie?(movie_instance)
+      reviewed = Review.all.select{|reviews|reviews.viewer == self}
+      review = reviewed.find{|reviews|reviews.movie == movie_instance}
+      review.rating = rating
+    else
+      Review.new(self, movie_instance, rating)
+    end
   end
 
   def self.all
